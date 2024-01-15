fix topic coherence computation

models/etm.py
@@ -1,19 +1,26 @@
 """@thematrixmaster
-Baseline class for training an ETM model. This class only implements the generative 
-model and does not include any inference methods. The class is meant to be extended
-by other classes that implement inference methods such as Gibbs sampling, variational
-expectation maximization or gflownets.
+Baseline class for training an ETM model with a Logistic Normal prior over \theta.
 """
 
 import torch
+from torchtyping import TensorType as TT
 import torch.nn.functional as F 
 from torch import nn
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class ETM(nn.Module):
-    def __init__(self, num_topics, vocab_size, t_hidden_size, rho_size,
-                    theta_act, embeddings=None, train_embeddings=True, enc_drop=0.5):
+    def __init__(
+        self,
+        num_topics: int,                    # number of topics
+        vocab_size: int,                    # size of vocabulary
+        t_hidden_size: int,                 # size of hidden layer in encoder
+        rho_size: int,                      # num of dimensions in word embedding
+        theta_act: str,                     # activation function for theta encoder
+        embeddings: TT["V", "L"] = None,    # pre-trained word embeddings
+        train_embeddings: bool = True,      # whether to train word embeddings from scratch
+        enc_drop=0.5                        # dropout rate for encoder
+    ):
         super(ETM, self).__init__()
 
         ## define hyperparameters
@@ -24,30 +31,32 @@ class ETM(nn.Module):
         self.enc_drop = enc_drop
         self.t_drop = nn.Dropout(enc_drop)
 
+        assert theta_act in ['tanh', 'relu', 'softplus', 'rrelu', 'leakyrelu', 'elu', 'selu', 'glu'], \
+            'theta_act should be in "tanh, relu, softplus, rrelu, leakyrelu, elu, selu, glu"'
+        
         self.theta_act = self.get_activation(theta_act)
 
-        self.train_embeddings = train_embeddings        
-        
-        ## define the word embedding matrix \rho
+        ## define the V x L word embedding matrix \rho
+        self.train_embeddings = train_embeddings
         if self.train_embeddings:
-            self.rho = nn.Parameter(torch.randn(vocab_size, rho_size)) # V x L
+            self.rho = nn.Parameter(torch.randn(vocab_size, rho_size))
         else:
-            self.rho = embeddings.clone().float().to(device) # V x L
+            self.rho = embeddings.clone().float().to(device)
 
-        ## define the matrix containing the topic embeddings
-        self.alphas = nn.Linear(rho_size, num_topics, bias=False)#nn.Parameter(torch.randn(rho_size, num_topics))
+        ## define the K x L topic embedding matrix \alpha
+        self.alphas = nn.Linear(rho_size, num_topics, bias=False)
     
-        ## define variational distribution for \theta_{1:D} via amortizartion
+        ## define the encoder that outputs variational parameters for \theta conditioned on x
         self.q_theta = nn.Sequential(
-                nn.Linear(vocab_size, t_hidden_size), 
-                self.theta_act,
-                nn.Linear(t_hidden_size, t_hidden_size),
-                self.theta_act,
-            )
+            nn.Linear(vocab_size, t_hidden_size), 
+            self.theta_act,
+            nn.Linear(t_hidden_size, t_hidden_size),
+            self.theta_act,
+        )
         self.mu_q_theta = nn.Linear(t_hidden_size, num_topics, bias=True)
         self.logsigma_q_theta = nn.Linear(t_hidden_size, num_topics, bias=True)
 
-    def get_activation(self, act):
+    def get_activation(self, act: str):
         if act == 'tanh':
             act = nn.Tanh()
         elif act == 'relu':
@@ -93,7 +102,7 @@ class ETM(nn.Module):
         mu_theta = self.mu_q_theta(q_theta)
         logsigma_theta = self.logsigma_q_theta(q_theta)
 
-        # KL[q(theta)||p(theta)] = lnq(theta) - lnp(theta)
+        # KL[q(theta) || p(theta)] = lnq(theta) - lnp(theta)
         kl_theta = -0.5 * torch.sum(1 + logsigma_theta - mu_theta.pow(2) - logsigma_theta.exp(), dim=-1).mean()
         return mu_theta, logsigma_theta, kl_theta
 

models/lda_gfn.py
@@ -97,29 +97,11 @@ class GFlowNet_EM(nn.Module):
         self.uniform_pb = uniform_pb
         self.model = NeuralNet(
             input_dim=2*K+1+self.context_dim,
-            output_dim=K*(2+3*n_mixture_components),
+            output_dim=K*(2+3*n_mixture_components+1),
             hidden_dim=hidden_dim,
             n_hidden_layers=n_hidden_layers,
             activation_fn=activation_fn
         )
-        self.logZ = nn.Parameter(torch.tensor(init_logZ), requires_grad=True)
-
-    def get_opt_params(self, lr=1e-3, Z_lr=0.1) -> [dict]:
-        """Gets the parameters to optimize for the gflownet policy"""
-        params = [{
-            "params": [
-                v for k, v in dict(self.named_parameters()).items() if k != "logZ"
-            ],
-            "lr": lr,
-        }]
-
-        if Z_lr > 0:
-            params.append({
-                "params": [dict(self.named_parameters())["logZ"]],
-                "lr": Z_lr,
-            })
-
-        return params
 
     def forward(
         self,
@@ -130,7 +112,8 @@ class GFlowNet_EM(nn.Module):
     ) -> (
         TT["num_docs", "K"],
         TT["num_docs", "K"],
-        TT["num_docs", "K", "n_mixture_components", 3]
+        TT["num_docs", "K", "n_mixture_components", 3],
+        TT["num_docs"]
     ):
         """
         Samples a batch of transitions
@@ -146,6 +129,7 @@ class GFlowNet_EM(nn.Module):
             pf_logits: logits for the forwards policy Pf(s_t|s_{t-1})
             pf_mixture_params: logits and beta dist parameters (alpha, beta) for 
                                 all K*M mixture components
+            logZ: the flow normalizing constant
         """
         x = torch.concat([theta, theta_mask, remaining.unsqueeze(1), context], dim=1)
         x = self.model(x)
@@ -159,7 +143,7 @@ class GFlowNet_EM(nn.Module):
         pf_mixture_params = x[:, 2*self.K:self.K*(2+3*self.n_mixture_components)]
         pf_mixture_params = pf_mixture_params.view(-1, self.K, self.n_mixture_components, 3)
 
-        return pb_logits, pf_logits, pf_mixture_params
+        return pb_logits, pf_logits, pf_mixture_params, x[:, -1]
     
     def sample_trajectories(
         self,
@@ -168,6 +152,7 @@ class GFlowNet_EM(nn.Module):
         epsilon: float = 0.,
     ) -> (
         TT["num_docs", "K"],
+        TT["num_docs"],
         TT["num_docs"],
         TT["num_docs"]
     ):
@@ -193,11 +178,15 @@ class GFlowNet_EM(nn.Module):
 
         for step in range(self.K+1):
 
-            pb_logits, pf_logits, pf_mixture_params = self.forward(
+            pb_logits, pf_logits, pf_mixture_params, log_flow = self.forward(
                 theta, theta_mask, remaining, context
             )
 
-            if step > 0:
+            if step == 0:
+                # we use the first step estimate for the flow normalizing constant Z
+                # since conditional gfn have different normalizing constants for each conditional flow
+                logZ = log_flow
+            elif step > 0:
                 # we gather the backwards transition probabilities from the parent 
                 # positions chosen in the previous step into the logPB estimate
                 logPB += pb_logits.gather(1, cur_topic_idx).squeeze(1)
@@ -287,7 +276,7 @@ class GFlowNet_EM(nn.Module):
                 # Update the set mask
                 theta_mask.scatter_(1, cur_topic_idx, torch.ones(num_docs,1).to(device))
         
-        return theta, logPF, logPB
+        return theta, logPF, logPB, logZ
     
 
 class LDA_GFN(LDA):
@@ -323,7 +312,6 @@ class LDA_GFN(LDA):
         activation_fn="elu",
         gfn_lr=1e-4,
         phi_lr=1e-4,
-        Z_lr=0.1,
         **kwargs,
     ) -> None:
         super().__init__(K=K, docs=docs, **kwargs)
@@ -344,8 +332,7 @@ class LDA_GFN(LDA):
         ).to(device)
 
         # initialize the optimizer for the gflownet policy and the word-topic mixture
-        params = self.policy.get_opt_params(lr=gfn_lr, Z_lr=Z_lr)
-        self.gfn_optimizer = optim.Adam(params, betas=(0.9, 0.999))
+        self.gfn_optimizer = optim.Adam(self.policy.parameters(), betas=(0.9, 0.999))
         self.phi_optimizer = optim.Adam([self.phi], lr=phi_lr, betas=(0.9, 0.999))
 
     def loglikelihood(
@@ -392,12 +379,12 @@ class LDA_GFN(LDA):
         for it in range(self.n_iter):
             # E-step
             for _ in range(100):
-                theta, logPF, logPB = self.policy.sample_trajectories(self.n_dw, epsilon=0.1)
+                theta, logPF, logPB, logZ = self.policy.sample_trajectories(self.n_dw, epsilon=0.1)
                 logR = self.loglikelihood(theta, self.phi, self.n_dw)
                 self.gfn_optimizer.zero_grad()
                 
                 # step on TB loss with respect to the gfn policy parameters
-                loss = ((self.policy.logZ + logPF - logR - logPB)**2).mean()
+                loss = ((logZ + logPF - logR - logPB)**2).mean()
                 loss.backward()
                 self.gfn_optimizer.step()
                 
@@ -447,7 +434,7 @@ class LDA_GFN(LDA):
                     log-rewards={np.array(log_rewards[-self.eval_every:]).mean()},
                     max-reward={np.max(log_rewards[-self.eval_every:])},
                     update-ratio={np.array(updates[-self.eval_every:]).mean()},
-                    logZ={self.policy.logZ.item()},
+                    logZ={logZ.mean().item()},
                     topic-coherence={tc[-1]},
                     topic-diversity={td[-1]}
                 """)

models/lda_gibbs.py
@@ -6,7 +6,11 @@ import torch
 import numpy as np
 from models.lda import LDA
 from tqdm import tqdm
-from utils.metrics import compute_topic_diversity, compute_topic_coherence, tokenize_docs
+from utils.metrics import (
+    compute_topic_diversity,
+    compute_topic_coherence,
+    tokenize_docs
+)
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
@@ -132,7 +136,12 @@ class LDA_Gibbs(LDA):
                 tc.append(compute_topic_coherence(phi, self.tok_docs, self.r_vocab, k=10))
                 td.append(compute_topic_diversity(phi, k=25))
 
-                print(f"Log likelihood: {ll[-1]}, Topic quality: {tc[-1]*td[-1]}")
+                print(f"""
+                    Log likelihood: {ll[-1]},
+                    Topic quality: {tc[-1]*td[-1]},
+                    Topic coherence: {tc[-1]},
+                    Topic diversity: {td[-1]}
+                    """)
 
                 if self.patience is None:
                     continue

utils/metrics.py
@@ -4,6 +4,7 @@ Compute metrics for evaluating topic models
 
 import numpy as np
 
+
 def tokenize_docs(docs: [[str]], vocab: {str: int}) -> [[int]]:
     """
     Tokenizes a list of documents into a list of lists of tokens
@@ -17,6 +18,7 @@ def tokenize_docs(docs: [[str]], vocab: {str: int}) -> [[int]]:
     """
     return [[vocab[word] for word in doc] for doc in docs]
 
+
 def top_k_words(phi: np.ndarray, r_vocab: {int: str}, k: int=10):
     """
     Returns the top k words for each topic using phi which 
@@ -26,6 +28,7 @@ def top_k_words(phi: np.ndarray, r_vocab: {int: str}, k: int=10):
     topk = np.argsort(phi, axis=1)[:, -k:]
     return [[(r_vocab[i], i) for i in row] for row in topk]
 
+
 def top_k_docs(theta: np.ndarray, doc_ids: [int], k: int=10):
     """
     Returns the top k documents for each topic using theta which
@@ -34,6 +37,12 @@ def top_k_docs(theta: np.ndarray, doc_ids: [int], k: int=10):
     """
     topk = np.argsort(theta, axis=0)[-k:, :]
     return [[(doc_ids[i], i) for i in row] for row in topk]
+
+
+def nearest_neighbors(model, word):
+    nearest_neighbors = model.wv.most_similar(word, topn=20)
+    nearest_neighbors = [comp[0] for comp in nearest_neighbors]
+    return nearest_neighbors
     
 
 def compute_topic_diversity(phi: np.ndarray, k: int=25):
@@ -46,6 +55,7 @@ def compute_topic_diversity(phi: np.ndarray, k: int=25):
     unique = np.unique(topk)
     return len(unique) / (phi.shape[0] * k)
 
+
 def normalized_mutual_information(w_i: int, w_j: int, docs: [[int]]):
     """
     Computes the normalized mutual information between two words $w_i$ and $w_j$
@@ -57,12 +67,14 @@ def normalized_mutual_information(w_i: int, w_j: int, docs: [[int]]):
     same document, and $P(w_i)$ and $P(w_j)$ are the probabilities of $w_i$ and $w_j$
     appearing in any document.
     """
-    P_wi = np.sum([w_i in doc for doc in docs]) / len(docs)
-    P_wj = np.sum([w_j in doc for doc in docs]) / len(docs)
-    P_wi_wj = np.sum([(w_i in doc and w_j in doc) for doc in docs]) / len(docs)
-    if P_wi_wj == 0:
-        return 0
-    return np.log(P_wi_wj / (P_wi * P_wj)) / -np.log(P_wi_wj)
+    D = len(docs)
+    N_wi = np.sum([w_i in doc for doc in docs])
+    N_wj = np.sum([w_j in doc for doc in docs])
+    N_wi_wj = np.sum([(w_i in doc and w_j in doc) for doc in docs])
+    if N_wi_wj == 0:
+        return -1
+    return -1 + (np.log(N_wi) + np.log(N_wj) - 2 * np.log(D)) / (np.log(N_wi_wj) - np.log(D))
+
 
 def compute_topic_coherence(phi: np.ndarray, docs: [[int]], r_vocab: {int: str}, k: int=10):
     """
@@ -74,14 +86,15 @@ def compute_topic_coherence(phi: np.ndarray, docs: [[int]], r_vocab: {int: str},
     TC = \frac{1}{K}\sum_{k=1}^{K}\frac{1}{45}\sum_{i=1}^{10}\sum_{j=1}^{10} f(w_i^{(k)}, w_j^{(k)})
     $$
     """
+    num_topics = phi.shape[0]
     topk = top_k_words(phi, r_vocab, k)
     tc = 0
-    for i in range(phi.shape[0]):
-        topic = topk[i]
-        for j in range(k):
-            for l in range(j+1, k):
-                tc += normalized_mutual_information(topic[j][1], topic[l][1], docs)
+    for i in range(num_topics):
+        top_words = topk[i]
+        for j in range(len(top_words)):
+            for l in range(j+1, len(top_words)):
+                tc += normalized_mutual_information(top_words[j][1], top_words[l][1], docs)
 
-        tc /= 45
-    return tc / k
+    norm = (k * (k-1)) / 2
+    return tc / (num_topics * norm)
     